575229d076b3c786972a7b1e4aa5f02cc9aa6eae
Arnold deVos <adv+gh@backgroundsignal.com>
2024-11-27 10:39:06 +1100

    Revert "attempt to wedge Codec and LogAdapter into streambed-patterns"
    This reverts commit 5c403cc4ed7107b1898ef8bdaca920487e6dd3bb.

--- streambed-patterns/src/lib.rs
+++ streambed-patterns/src/lib.rs
@@ -1,10 +1,3 @@
 #![doc = include_str!("../README.md")]
 
-#[cfg(feature = "ask")]
 pub mod ask;
-
-#[cfg(feature = "log-adapter")]
-pub mod log_adapter;
-
-#[cfg(feature = "codec")]
-pub mod codec;
