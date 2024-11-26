5c403cc4ed7107b1898ef8bdaca920487e6dd3bb
Arnold deVos <adv+gh@backgroundsignal.com>
2024-11-26 11:48:09 +1100

    attempt to wedge Codec and LogAdapter into streambed-patterns

--- streambed-patterns/src/lib.rs
+++ streambed-patterns/src/lib.rs
@@ -1,3 +1,10 @@
 #![doc = include_str!("../README.md")]
 
+#[cfg(feature = "ask")]
 pub mod ask;
+
+#[cfg(feature = "log-adapter")]
+pub mod log_adapter;
+
+#[cfg(feature = "codec")]
+pub mod codec;
